7d91852569f75a73196f8ed1c036981442a121dd
root <root@ip-172-31-3-26.ec2.internal>
2022-05-03 10:31:19 +0000

    apache configuration added

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -6,6 +6,16 @@ class mymodule::httpd {
        ensure => running,
        enable => true,
        required => Package["httpd"],
- }  
+ }
+ file {'/etc/httpd/conf.d/myserver.conf':
+  notify => service['httpd],
+  ensure => file,
+  required => Package["httpd],
+  content => template("mymodule/myserver.conf.erb"),
+ }
+ file {"/var/www/myserver":
+   ensure => "directory",  
+}
+  
 }
 
